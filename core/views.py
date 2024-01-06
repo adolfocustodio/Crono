6d6526462fd5ab5e96c719af6089fa7ac87ab9ed
adolfocustodio <adolfocustodio14@gmail.com>
2024-01-06 17:03:02 -0300

    home page

--- core/views.py
+++ core/views.py
@@ -1,1 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
+from posts.models import Post
+
+class Home(generic.ListView):
+    template_name = 'core/home.html'
+    model = Post
+    context_object_name = 'posts'
