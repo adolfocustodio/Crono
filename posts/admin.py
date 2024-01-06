f330614d6f08c32df013edbd6e8b74acdf208ca4
adolfocustodio <adolfocustodio14@gmail.com>
2024-01-06 00:36:01 -0300

    post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc_preview', 'category')
+
+    def desc_preview(self, obj):
+        return obj.desc[:100] + '...'
--- posts/models.py
+++ posts/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from categories.models import Category
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    desc = models.TextField()
+    file = models.FileField(upload_to='files', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
